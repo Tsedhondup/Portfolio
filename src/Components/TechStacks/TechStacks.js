9470d3bd1cf560442b7e70cb20574ebc9e0ab554
Tsering Dhondup <tyddhondup88@gmail.com>
2024-07-29 18:06:27 -0400

    TechStacks component created

---
+++ src/Components/TechStacks/TechStacks.js
@@ -0,0 +1,15 @@
+const TechStacks = () => {
+  return (
+    <section>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React.js</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+      </ul>
+    </section>
+  );
+};
+export default TechStacks;
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import LandingDisplay from "../../Components/LandingDisplay/LandingDisplay";
 import AboutSecond from "../../Components/AboutSecond/AboutSecond";
+import TechStacks from "../../Components/TechStacks/TechStacks";
 const HomePage = () => {
   return (
     <>
       <LandingDisplay />
       <AboutSecond />
+      <TechStacks />
     </>
   );
 };
