72c7f2e21bbc9fb1cc406a147062794292a605b4
Tsering Dhondup <tyddhondup88@gmail.com>
2024-07-28 22:35:40 -0400

    Header component created

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <section>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/About">About</Link>
+        <span>Resume</span>
+      </nav>
+    </section>
+  );
+};
+
+export default Header;
