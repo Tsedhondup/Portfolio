4e65320c04ed24706a4e07e4c5f3b330733874f3
Tsering Dhondup <tyddhondup88@gmail.com>
2024-07-28 21:37:45 -0400

    Home and about page created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import HomePage from "./Pages/HomePage/HomePage";
+import AboutPage from "./Pages/AboutPage/AboutPage";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/About" element={<AboutPage />} />
+          </Routes>
+        </BrowserRouter>
+      </>
     </div>
   );
 }
