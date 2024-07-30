464f73019f5247cdce02e0ee3847ffebed0e5d03
Tsering Dhondup <tyddhondup88@gmail.com>
2024-07-29 20:58:12 -0400

    Works Component created

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,12 +1,14 @@
 import LandingDisplay from "../../Components/LandingDisplay/LandingDisplay";
 import AboutSecond from "../../Components/AboutSecond/AboutSecond";
 import TechStacks from "../../Components/TechStacks/TechStacks";
+import Works from "../../Components/Works/Works";
 const HomePage = () => {
   return (
     <>
       <LandingDisplay />
       <AboutSecond />
       <TechStacks />
+      <Works />
     </>
   );
 };
