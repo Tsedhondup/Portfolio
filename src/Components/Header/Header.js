0fd3a38b0fd9f34cdcb3919e057d49ea4cf84c9e
Tsering Dhondup <tyddhondup88@gmail.com>
2024-07-31 22:31:42 -0400

    class names added to Header component elements

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,25 @@
 import { Link } from "react-router-dom";
+import "./Header.scss";
 const Header = () => {
   return (
     <section>
-      <nav>
-        <Link to="/">TD</Link>
-        <Link to="/">Home</Link>
-        <Link to="/About">About</Link>
-        <span>Resume</span>
+      <nav className="navigation">
+        <Link to="/" className="navigation__brand-name">
+          TD
+        </Link>
+        <div className="navigtion-pages">
+          <Link to="/" className="navigation-pages__home">
+            Home
+          </Link>
+          <Link to="/About" className="navigation-pages__about">
+            About
+          </Link>
+          <span className="navigations-pages__resume">Resume</span>
+        </div>
+        <div className="nav-button-container">
+          <button className="nav-button-container__open">open</button>
+          <button className="nav-button-container__close">close</button>
+        </div>
       </nav>
     </section>
   );
