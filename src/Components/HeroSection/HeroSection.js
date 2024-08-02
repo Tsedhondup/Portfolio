dadabc2354e4bb40b646032c89eac7c9d7a78365
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-01 20:47:12 -0400

    class names added to HeroComponent elements

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -1,10 +1,10 @@
 import "./HeroSection.scss";
-const LandingDisplay = () => {
+const HeroSection = () => {
   return (
     <section className="hero-container">
-      <h1>Tsering Dhondup</h1>
-      <h3>Web developer</h3>
+      <h1 className="hero-container__nameF">Tsering Dhondup</h1>
+      <h3 className="hero-container__title">Web developer</h3>
     </section>
   );
 };
-export default LandingDisplay;
+export default HeroSection;
