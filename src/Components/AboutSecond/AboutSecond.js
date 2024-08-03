51b804244878a50063bb0d947ad4a7d83d49eea8
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-03 13:48:47 -0400

    class names added to AboutSecond component element

--- src/Components/AboutSecond/AboutSecond.js
+++ src/Components/AboutSecond/AboutSecond.js
@@ -1,16 +1,20 @@
 import profileImage from "../../assets/image/profile.png";
+import "./AboutSecond.scss";
 const AboutSecond = () => {
   return (
-    <section>
-      <p>
+    <section className="about-second">
+      <img
+        src={profileImage}
+        alt="profile-image"
+        className="about-second__img"
+      />
+      <p className="about-second__texts">
         From a simple HTML layout to complex web applications, I've always been
         fascinated by the power of the web to connect, inform, and inspire. What
         started as a curiosity and hobby has evolved into a full-fledged
         passion, driving me to pursue a career transition into the tech industry
         where I can play a part in shaping the digital landscape
       </p>
-
-      <img src={profileImage} alt="profile-image" />
     </section>
   );
 };
