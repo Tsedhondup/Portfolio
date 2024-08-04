9c64db6a3545d02b009ae4b20e071bc50b4a091a
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-04 15:43:37 -0400

    Work component Code refactored

--- src/Components/Works/Works.js
+++ src/Components/Works/Works.js
@@ -2,34 +2,34 @@ const Works = () => {
   return (
     <section className="project-container">
       <section className="projects">
+        <p className="projects__thumbnail">Weather Icon/Image</p>
         <h2 className="projects__header">Weather App</h2>
         <p className="projects__description">
           BrainStation Projects built on Reacts.js
         </p>
-        <p className="projects__thumbnail">InStock Icon/Image</p>
       </section>
       <section className="projects">
+        <p className="projects__thumbnail">BrainFlix Icon/Image</p>
         <h2 className="projects__header">BrainFlix</h2>
         <p className="projects__description">
           BrainStation Projects built on Reacts.js
         </p>
-        <p className="projects__thumbnail">BrainFlix Icon/Image</p>
       </section>
       <section className="projects">
+        <p className="projects__thumbnail">
+          Job Application Tracker Icon/Image
+        </p>
         <h2 className="projects__header">Job Application Tracker</h2>
         <p className="projects__description">
           BrainStation Projects built on Reacts.js
         </p>
-        <p className="projects__thumbnail">
-          Job Application Tracker Icon/Image
-        </p>
       </section>
       <section className="projects">
+        <p className="projects__thumbnail">InStock Icon/Image</p>
         <h2 className="projects__header">InStock</h2>
         <p className="projects__description">
           BrainStation Projects built on Reacts.js
         </p>
-        <p className="projects__thumbnail">InStock Icon/Image</p>
       </section>
     </section>
   );
