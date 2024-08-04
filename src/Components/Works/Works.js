ab5660276598af84f4f0db22744a110a7f669210
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-04 16:03:27 -0400

    mobile styles for project section completed

--- src/Components/Works/Works.js
+++ src/Components/Works/Works.js
@@ -1,16 +1,18 @@
+import "./Works.scss";
 const Works = () => {
   return (
     <section className="project-container">
+      <h2 className="project-container__title">Projects</h2>
       <section className="projects">
         <p className="projects__thumbnail">Weather Icon/Image</p>
-        <h2 className="projects__header">Weather App</h2>
+        <h3 className="projects__header">Weather App</h3>
         <p className="projects__description">
           BrainStation Projects built on Reacts.js
         </p>
       </section>
       <section className="projects">
         <p className="projects__thumbnail">BrainFlix Icon/Image</p>
-        <h2 className="projects__header">BrainFlix</h2>
+        <h3 className="projects__header">BrainFlix</h3>
         <p className="projects__description">
           BrainStation Projects built on Reacts.js
         </p>
@@ -19,14 +21,14 @@ const Works = () => {
         <p className="projects__thumbnail">
           Job Application Tracker Icon/Image
         </p>
-        <h2 className="projects__header">Job Application Tracker</h2>
+        <h3 className="projects__header">Job Application Tracker</h3>
         <p className="projects__description">
           BrainStation Projects built on Reacts.js
         </p>
       </section>
       <section className="projects">
         <p className="projects__thumbnail">InStock Icon/Image</p>
-        <h2 className="projects__header">InStock</h2>
+        <h3 className="projects__header">InStock</h3>
         <p className="projects__description">
           BrainStation Projects built on Reacts.js
         </p>
