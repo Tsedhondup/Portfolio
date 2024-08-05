eede25f6d18d7cb9950e509343bfa6147c1778d6
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-05 19:56:59 -0400

    border-bottom set to about-second text

--- src/Components/AboutSecond/AboutSecond.js
+++ src/Components/AboutSecond/AboutSecond.js
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStarOfLife } from "@fortawesome/free-solid-svg-icons";
 import profileImage from "../../assets/image/tsering.png";
 import "./AboutSecond.scss";
 const AboutSecond = () => {
@@ -9,11 +11,15 @@ const AboutSecond = () => {
         alt="profile-image"
         className="about-second__img"
       />
+      <FontAwesomeIcon
+        icon={faStarOfLife}
+        className="about-second__star-of-life"
+      />
       <p className="about-second__texts">
-        I've always been fascinated by the power of the web to connect, inform,
+        "I've always been fascinated by the power of the web to connect, inform,
         and inspire. What started as a curiosity and hobby has evolved into a
         full-fledged passion, driving me to pursue a career transition into the
-        tech industry where I can play a part in shaping the digital landscape
+        tech industry where I can play a part in shaping the digital landscape."
         <Link to="/About" className="about-second__link">
           Read more
         </Link>
