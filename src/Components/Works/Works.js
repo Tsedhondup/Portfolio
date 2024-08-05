6b06bcd42d299cca1d65bb6bf6a33776e1183d64
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-04 20:39:32 -0400

    projects size changed

--- src/Components/Works/Works.js
+++ src/Components/Works/Works.js
@@ -11,14 +11,22 @@ const Works = () => {
     <section className="project-container">
       <h2 className="project-container__title">Projects</h2>
       <section className="projects">
-        <FontAwesomeIcon icon={faCloudSunRain} className="projects__icon" />
+        <FontAwesomeIcon
+          icon={faCloudSunRain}
+          transform="grow-25"
+          className="projects__icon"
+        />
         <h3 className="projects__header">Weather App</h3>
         <p className="projects__description">
           BrainStation Projects built on Reacts.js
         </p>
       </section>
       <section className="projects">
-        <FontAwesomeIcon icon={faCirclePlay} className="projects__icon" />
+        <FontAwesomeIcon
+          icon={faCirclePlay}
+          transform="grow-25"
+          className="projects__icon"
+        />
         <h3 className="projects__header">BrainFlix</h3>
         <p className="projects__description">
           BrainStation Projects built on Reacts.js
@@ -27,6 +35,7 @@ const Works = () => {
       <section className="projects">
         <FontAwesomeIcon
           icon={faEnvelopeCircleCheck}
+          transform="grow-25"
           className="projects__icon"
         />
 
@@ -36,7 +45,11 @@ const Works = () => {
         </p>
       </section>
       <section className="projects">
-        <FontAwesomeIcon icon={faCartFlatbed} className="projects__icon" />
+        <FontAwesomeIcon
+          icon={faCartFlatbed}
+          transform="grow-25"
+          className="projects__icon"
+        />
 
         <h3 className="projects__header">InStock</h3>
         <p className="projects__description">
