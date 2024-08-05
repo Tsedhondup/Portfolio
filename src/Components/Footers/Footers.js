351ec65d2e081b5837034eddfb2d8c747c3a83c1
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-05 14:10:45 -0400

    class names added to footer component's elements

--- src/Components/Footers/Footers.js
+++ src/Components/Footers/Footers.js
@@ -11,17 +11,23 @@ import "./Footers.scss";
 const Footers = () => {
   return (
     <footer className="footer">
-      <h3>TseringDhondup</h3>
-      <h3>Toronto</h3>
-      <h3>
-        copyright2024 <FontAwesomeIcon icon={faCopyright} />
-      </h3>
       <div className="socials">
         <FontAwesomeIcon icon={faLinkedin} className="socials__linkedIn" />
         <FontAwesomeIcon icon={faGithub} className="socials__github" />
         <FontAwesomeIcon icon={faTwitter} className="socials__x-twitter" />
         <FontAwesomeIcon icon={faFacebook} className="socials__facebook" />
         <FontAwesomeIcon icon={faInstagram} className="socials__instagram" />
+      </div>
+      <div className="author">
+        <h3 className="author__designer">Designed and built by </h3>
+        <h3 className="author__name">TseringDhondup</h3>
+      </div>
+
+      <div className="location-copyright">
+        <h3 className="location-copyright__copyright">
+          copyright2024 <FontAwesomeIcon icon={faCopyright} />
+        </h3>
+        <h3 className="location-copyright__location">Toronto</h3>
       </div>
     </footer>
   );
