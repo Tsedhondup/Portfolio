9c22b2a2ce19a9287bc928c92a8c577f470cae70
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-05 13:18:40 -0400

    copyright icons imported

--- src/Components/Footers/Footers.js
+++ src/Components/Footers/Footers.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { faCopyright } from "@fortawesome/free-solid-svg-icons";
 import {
   faGithub,
   faLinkedin,
@@ -7,11 +7,15 @@ import {
   faInstagram,
   faFacebook,
 } from "@fortawesome/free-brands-svg-icons";
+import "./Footers.scss";
 const Footers = () => {
   return (
     <footer>
       <h3>TseringDhondup</h3>
       <h3>Toronto</h3>
+      <h3>
+        copyright2024 <FontAwesomeIcon icon={faCopyright} />
+      </h3>
       <div className="socials">
         <FontAwesomeIcon icon={faLinkedin} className="socials__linkedIn" />
         <FontAwesomeIcon icon={faGithub} className="socials__github" />
