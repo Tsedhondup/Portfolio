5eaa9b82708350952401b3d746ca6db18d8cefe0
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-05 18:53:07 -0400

    icons for tech stacks imported

--- src/Components/TechStacks/TechStacks.js
+++ src/Components/TechStacks/TechStacks.js
@@ -1,19 +1,33 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHtml5,
+  faCss3,
+  faJs,
+  faReact,
+  faNodeJs,
+  faSass,
+} from "@fortawesome/free-brands-svg-icons";
+import expressLogo from "../../assets/image/express.png";
+import mysqlLogo from "../../assets/image/mysql.png";
+import knexLogo from "../../assets/image/knex.png";
+
 import "./TechStacks.scss";
 const TechStacks = () => {
   return (
     <section className="tech-stacks">
       <h2 className="tech-stacks__header">Primary Tech stacks</h2>
-      <ul className="tech-stacks__tools">
-        <li className="tech-stacks__tools--tech">HTML</li>
-        <li className="tech-stacks__tools--tech">CSS</li>
-        <li className="tech-stacks__tools--tech">SASS</li>
-        <li className="tech-stacks__tools--tech">JavaScript</li>
-        <li className="tech-stacks__tools--tech">React.js</li>
-        <li className="tech-stacks__tools--tech">Node.js</li>
-        <li className="tech-stacks__tools--tech">Express.js</li>
-        <li className="tech-stacks__tools--tech">SQL</li>
-        <li className="tech-stacks__tools--tech">Knex.js</li>
-      </ul>
+      <div className="tech-stacks__tools">
+        <FontAwesomeIcon icon={faHtml5} className="tech-stacks__tools--tech" />
+        <FontAwesomeIcon icon={faCss3} className="tech-stacks__tools--tech" />
+        <FontAwesomeIcon icon={faSass} className="tech-stacks__tools--tech" />
+        <FontAwesomeIcon icon={faJs} className="tech-stacks__tools--tech" />
+        <FontAwesomeIcon icon={faReact} className="tech-stacks__tools--tech" />
+        <FontAwesomeIcon icon={faNodeJs} className="tech-stacks__tools--tech" />
+        <img src={expressLogo} />
+        <img src={mysqlLogo} />
+        <img src={knexLogo} />
+
+      </div>
     </section>
   );
 };
