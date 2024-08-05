0e5672761ae28549b3a00ff9aadb142db294ceca
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-05 13:11:34 -0400

    phone and email link added

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -10,16 +10,20 @@ const Contacts = () => {
     <section className="contacts">
       <h2 className="contacts__header">Contact me</h2>
       <div className="phone-email">
-        <FontAwesomeIcon
-          icon={faMobileScreenButton}
-          transform="grow-10"
-          className="phone-email__phone"
-        />
-        <FontAwesomeIcon
-          icon={faEnvelope}
-          transform="grow-10"
-          className="phone-email__email"
-        />
+        <a href="tel:+1437245778">
+          <FontAwesomeIcon
+            icon={faMobileScreenButton}
+            transform="grow-10"
+            className="phone-email__phone"
+          />
+        </a>
+        <a href="mailto:tyddhondup88@gmail.com">
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            transform="grow-10"
+            className="phone-email__email"
+          />
+        </a>
       </div>
 
       <div className="contacts__translucent-background"></div>
