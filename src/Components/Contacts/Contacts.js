ff22b0841c95fca62ae1f4f4e12be6ed7809a1bc
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-05 01:57:18 -0400

    base style added to contacts header, phone and email

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -14,7 +14,7 @@ import "./Contacts.scss";
 const Contacts = () => {
   return (
     <section className="contacts">
-      <h2>Say hello to me!</h2>
+      <h2 className="contacts__header">Say hello to me!</h2>
       <div className="phone-email">
         <FontAwesomeIcon
           icon={faMobileScreenButton}
