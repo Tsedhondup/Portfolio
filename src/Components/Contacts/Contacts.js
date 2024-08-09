55e34f837b8fa3ecb4bd3c4ef0655f5263d864bb
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-09 16:16:00 -0400

    Contact Component code refactored

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -10,20 +10,36 @@ const Contacts = () => {
     <section className="contacts">
       <h2 className="contacts__header">Contact me</h2>
       <div className="phone-email">
-        <a href="tel:+1437245778">
-          <FontAwesomeIcon
-            icon={faMobileScreenButton}
-            transform="grow-10"
-            className="phone-email__phone"
-          />
-        </a>
-        <a href="mailto:tyddhondup88@gmail.com">
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            transform="grow-10"
-            className="phone-email__email"
-          />
-        </a>
+        <div className="phone-email__phone-container">
+          <a href="tel:+1437245778">
+            <FontAwesomeIcon
+              icon={faMobileScreenButton}
+              transform="grow-10"
+              className="phone-email__phone-container--icon"
+            />
+          </a>
+          <a
+            href="tel:+1437245778"
+            className="phone-email__phone-container--text"
+          >
+            call
+          </a>
+        </div>
+        <div className="phone-email__email-container">
+          <a href="mailto:tyddhondup88@gmail.com">
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              transform="grow-10"
+              className="phone-email__email-container--text"
+            />
+          </a>
+          <a
+            href="mailto:tyddhondup88@gmail.com"
+            className="phone-email__email-container--icon"
+          >
+            email
+          </a>
+        </div>
       </div>
 
       <div className="contacts__translucent-background"></div>
