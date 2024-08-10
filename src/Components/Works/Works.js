6e04decc84e20662815c34fef77a8d7d5da7fa39
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-10 16:50:24 -0400

    code refactored for work component

--- src/Components/Works/Works.js
+++ src/Components/Works/Works.js
@@ -10,51 +10,53 @@ const Works = () => {
   return (
     <section className="project-container">
       <h2 className="project-container__title">Projects</h2>
-      <section className="projects">
-        <FontAwesomeIcon
-          icon={faCloudSunRain}
-          transform="grow-15"
-          className="projects__icon"
-        />
-        <h3 className="projects__header">Weather App</h3>
-        <p className="projects__description">
-          BrainStation Projects built on Reacts.js
-        </p>
-      </section>
-      <section className="projects">
-        <FontAwesomeIcon
-          icon={faCirclePlay}
-          transform="grow-15"
-          className="projects__icon"
-        />
-        <h3 className="projects__header">BrainFlix</h3>
-        <p className="projects__description">
-          BrainStation Projects built on Reacts.js
-        </p>
-      </section>
-      <section className="projects">
-        <FontAwesomeIcon
-          icon={faEnvelopeCircleCheck}
-          transform="grow-15"
-          className="projects__icon"
-        />
+      <section className="project-container__project-content">
+        <section className="projects">
+          <FontAwesomeIcon
+            icon={faCloudSunRain}
+            transform="grow-15"
+            className="projects__icon"
+          />
+          <h3 className="projects__header">Weather App</h3>
+          <p className="projects__description">
+            BrainStation Projects built on Reacts.js
+          </p>
+        </section>
+        <section className="projects">
+          <FontAwesomeIcon
+            icon={faCirclePlay}
+            transform="grow-15"
+            className="projects__icon"
+          />
+          <h3 className="projects__header">BrainFlix</h3>
+          <p className="projects__description">
+            BrainStation Projects built on Reacts.js
+          </p>
+        </section>
+        <section className="projects">
+          <FontAwesomeIcon
+            icon={faEnvelopeCircleCheck}
+            transform="grow-15"
+            className="projects__icon"
+          />
 
-        <h3 className="projects__header">Job Application Tracker</h3>
-        <p className="projects__description">
-          BrainStation Projects built on Reacts.js
-        </p>
-      </section>
-      <section className="projects">
-        <FontAwesomeIcon
-          icon={faCartFlatbed}
-          transform="grow-15"
-          className="projects__icon"
-        />
+          <h3 className="projects__header">Job Application Tracker</h3>
+          <p className="projects__description">
+            BrainStation Projects built on Reacts.js
+          </p>
+        </section>
+        <section className="projects">
+          <FontAwesomeIcon
+            icon={faCartFlatbed}
+            transform="grow-15"
+            className="projects__icon"
+          />
 
-        <h3 className="projects__header">InStock</h3>
-        <p className="projects__description">
-          BrainStation Projects built on Reacts.js
-        </p>
+          <h3 className="projects__header">InStock</h3>
+          <p className="projects__description">
+            BrainStation Projects built on Reacts.js
+          </p>
+        </section>
       </section>
     </section>
   );
