1a6c6e52cc54b3cdd3306d89b29d94aecf06a1fa
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-10 15:53:22 -0400

    About second component code refactored

--- src/Components/AboutSecond/AboutSecond.js
+++ src/Components/AboutSecond/AboutSecond.js
@@ -6,28 +6,30 @@ import "./AboutSecond.scss";
 const AboutSecond = () => {
   return (
     <section className="about-second">
-      <div className="about-second__image-container">
-        <div className="about-second__image-container--shadow-element"></div>
-        <img
-          src={profileImage}
-          alt="profile-image"
-          className="about-second__image-container--img"
+      <section className="about-second-content">
+        <div className="about-second-content__image-container">
+          <div className="about-second-content__image-container--shadow-element"></div>
+          <img
+            src={profileImage}
+            alt="profile-image"
+            className="about-second-content__image-container--img"
+          />
+        </div>
+        <FontAwesomeIcon
+          icon={faStarOfLife}
+          className="about-second-content__star-of-life"
         />
-      </div>
-
-      <FontAwesomeIcon
-        icon={faStarOfLife}
-        className="about-second__star-of-life"
-      />
-      <p className="about-second__texts">
-        "I've always been fascinated by the power of the web to connect, inform,
-        and inspire. What started as a curiosity and hobby has evolved into a
-        full-fledged passion, driving me to pursue a career transition into the
-        tech industry where I can play a part in shaping the digital landscape."
-        <Link to="/About" className="about-second__link">
-          Read more
-        </Link>
-      </p>
+        <p className="about-second-content__texts">
+          I've always been fascinated by the power of the web to connect,
+          inform, and inspire. What started as a curiosity and hobby has evolved
+          into a full-fledged passion, driving me to pursue a career transition
+          into the tech industry where I can play a part in shaping the digital
+          landscape.
+          <Link to="/About" className="about-second-content__link">
+            Read more
+          </Link>
+        </p>
+      </section>
     </section>
   );
 };
