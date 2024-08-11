8201e479e60978b92831b4de2dccda00dd3aa7ba
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-11 14:47:18 -0400

    un-used function removed from About page component

--- src/Pages/AboutPage/AboutPage.js
+++ src/Pages/AboutPage/AboutPage.js
@@ -4,9 +4,7 @@ import Contacts from "../../Components/Contacts/Contacts";
 import { useState } from "react";
 const AboutPage = () => {
   const [isScroll, setIsScroll] = useState(false);
-  const pageScrollHandler = () => {
-    console.log("yes");
-  };
+
   return (
     <>
       <Header isScroll={isScroll} />
