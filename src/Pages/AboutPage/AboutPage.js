42f0bf9d89b0ca51a2a02df62e9e93d2f0ae187d
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-12 22:37:37 -0400

    windowWitdh state variable created

--- src/Pages/AboutPage/AboutPage.js
+++ src/Pages/AboutPage/AboutPage.js
@@ -4,8 +4,8 @@ import Contacts from "../../Components/Contacts/Contacts";
 import { useState } from "react";
 const AboutPage = () => {
   const [isScroll, setIsScroll] = useState(false);
-  const [headerClass, setHeaderClass] = useState("header-opaque-home-page");
-
+  const [headerClass, setHeaderClass] = useState("header-opaque-about-page");
+  const [windowWitdh, setWindowWitdh] = useState(window.innerWidth);
   return (
     <>
       <Header isScroll={isScroll} headerClass={headerClass} />
