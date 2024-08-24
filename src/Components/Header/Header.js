7258cacb114a181367064dfe8c760f57994d5cea
Tsering Dhondup <tyddhondup88@gmail.com>
2024-08-24 01:16:42 -0400

    drop down animation applied to navigation page links for mobile view

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,6 @@ import "./Header.scss";
 const Header = (props) => {
   const [navPageClass, setNavPageClass] = useState("");
   const [isNavPageShow, setIsNavPageShow] = useState(false);
-  const [elememtPosition, setElementPosition] = useState("");
   const elementRef = useRef(null);
   // event handlers
   const handleNavButton = () => {
@@ -20,7 +19,7 @@ const Header = (props) => {
       setIsNavPageShow(false);
     }
   };
-  const handleNavLinkPosition = () => {};
+
   return (
     <motion.section
       initial={{ y: -100 }}
@@ -50,47 +49,56 @@ const Header = (props) => {
         </Link>
         {/* Pages links */}
 
-        <div className={`navigation-pages ${navPageClass}`} ref={elementRef}>
+        <div className={`navigation-pages ${navPageClass}`}>
           <div className="navigation-pages__link-container">
             <motion.h2
-              initial={{ opacity: 0, borderBottomWidth: 0 }}
+              initial={{ opacity: 0 }}
               whileInView={{
                 opacity: 1,
-                borderWidth: 1,
-                transition: { delay: 2, duration: 1.5 },
+                transition: { delay: 1, duration: 1.5 },
               }}
               viewport={{ once: false, amount: 1 }}
               className="navigation-pages__link-container--author-name"
             >
               Tsering Dhondup
             </motion.h2>
-            <motion.Link
+            <motion.div
               initial={{ opacity: 0, borderBottomWidth: 0 }}
               whileInView={{
+                // opacity: elememtPosition <= 0 ? 1 : 0,
+                // borderBottomWidth: elememtPosition <= 0 ? 1 : 0,
                 opacity: 1,
-                borderWidth: 1,
+                borderBottomWidth: 1,
                 transition: { delay: 0.8, duration: 0.5 },
               }}
               viewport={{ once: false, amount: 1 }}
-              to="/"
-              className="navigation-pages__link-container--home"
-              onClick={handleNavButton}
             >
-              Home
-            </motion.Link>
-            <motion.Link
+              <Link
+                to="/"
+                className="navigation-pages__link-container--home"
+                onClick={handleNavButton}
+              >
+                Home
+              </Link>
+            </motion.div>
+            <motion.div
               initial={{ opacity: 0, borderBottomWidth: 0 }}
               whileInView={{
                 opacity: 1,
+                borderBottomWidth: 1,
                 transition: { delay: 0.6, duration: 0.5 },
               }}
               viewport={{ once: false, amount: 1 }}
-              to="/About"
-              className="navigation-pages__link-container--about"
-              onClick={handleNavButton}
             >
-              About
-            </motion.Link>
+              <Link
+                to="/About"
+                className="navigation-pages__link-container--about"
+                onClick={handleNavButton}
+              >
+                About
+              </Link>
+            </motion.div>
+
             <motion.a
               initial={{ opacity: 0, borderBottomWidth: 0 }}
               whileInView={{
@@ -109,7 +117,6 @@ const Header = (props) => {
             initial={{ opacity: 0, borderBottomWidth: 0 }}
             whileInView={{
               opacity: 1,
-              transition: { delay: 0.2, duration: 0.5 },
             }}
             viewport={{ once: false, amount: 1 }}
           >
@@ -123,10 +130,7 @@ const Header = (props) => {
 
           <div
             className="navigation-pages__shadow-element"
-            onClick={() => {
-              handleNavButton();
-              handleNavLinkPosition();
-            }}
+            onClick={handleNavButton}
           ></div>
         </div>
         {/* Toggle buttons */}
@@ -135,10 +139,7 @@ const Header = (props) => {
             icon={faBars}
             transform="grow-5"
             className="nav-button-container__open"
-            onClick={() => {
-              handleNavButton();
-              handleNavLinkPosition();
-            }}
+            onClick={handleNavButton}
           />
         </div>
       </motion.nav>
