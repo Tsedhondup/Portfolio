0d7960086848e2fe38c59fa35d71ca34b3ee843e
Tsering Dhondup <tyddhondup88@gmail.com>
2024-10-01 23:06:02 -0400

    background image for contact page now applied conditionally

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -7,7 +7,11 @@ import {
 import "./Contacts.scss";
 const Contacts = (props) => {
   return (
-    <section className={`${props.isHomePage ? "contact-home" : ""} contacts`}>
+    <section
+      className={`${
+        props.isHomePage ? "contact-home" : "contact-about"
+      } contacts`}
+    >
       <h2 className="contacts__header">Contact me</h2>
       <div className="phone-email">
         <div className="phone-email__phone-container">
