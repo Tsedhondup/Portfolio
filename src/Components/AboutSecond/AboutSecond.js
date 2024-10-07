3d73b69ceb1ec4353abf46d1ad531458ad369ba3
Tsering Dhondup <tyddhondup88@gmail.com>
2024-10-06 22:43:11 -0400

    text align changed for project description

--- src/Components/AboutSecond/AboutSecond.js
+++ src/Components/AboutSecond/AboutSecond.js
@@ -28,13 +28,14 @@ const AboutSecond = () => {
         <motion.p
           initial={{
             opacity: 0,
-            y: screenWidth < 767 || screenWidth >= 950 ? 100 : 0,
-            x: screenWidth >= 767 && screenWidth < 950 ? 100 : 0,
+            y: 60,
+            // y: screenWidth < 767 || screenWidth >= 950 ? 100 : 0,
+            // x: screenWidth >= 767 && screenWidth < 950 ? 100 : 0,
           }}
           whileInView={{
             opacity: 1,
             y: 0,
-            x: 0,
+            // x: 0,
             transition: { duration: 1 },
           }}
           viewport={{ once: true, amount: 1 }}
