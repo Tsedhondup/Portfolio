51aed1b8fc87db163ab81ea58411a64083b36fa0
Tsering Dhondup <tyddhondup88@gmail.com>
2024-10-16 00:07:24 -0400

    project image height and width updated

--- src/Components/Works/Works.js
+++ src/Components/Works/Works.js
@@ -19,68 +19,6 @@ const Works = () => {
       <motion.h2 className="project-container__title">Projects</motion.h2>
       <section className="projects">
         <motion.a
-          className="projects__content projects__content-email-reader"
-          href="https://github.com/Tsedhondup/job-application-tracker"
-          target="_blank"
-          onMouseEnter={() => setHoveredOne(true)}
-          onMouseLeave={() => setHoveredOne(false)}
-          initial={{
-            y: 60,
-            opacity: 0,
-          }}
-          whileInView={{
-            y: 0,
-            opacity: 1,
-            transition: { delay: 0.5, duration: 1, ease: "linear" },
-          }}
-          viewport={{ once: true, amount: 0.5 }}
-        >
-          <motion.img
-            initial={{ filter: "grayScale(100)", opacity: 0 }}
-            whileInView={{
-              filter: "grayScale(0)",
-              opacity: 1,
-              transition: { delay: 0.5, duration: 1, ease: "linear" },
-            }}
-            viewport={{ once: true, amount: 0.5 }}
-            src={jobTracker}
-            alt="job-application-tracker-image"
-            className="projects__content--img projects__content--email-image "
-          />
-          <div className="projects__content--text ">
-            <motion.h2
-              className={`projects__content--project-name projects__content--text-email ${
-                hoveredOne ? "js-color-pewter" : ""
-              }`}
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{
-                opacity: 1,
-                y: 0,
-                transition: { delay: 1.5, duration: 1, ease: "linear" },
-              }}
-              viewport={{ once: true, amount: 0.5 }}
-            >
-              Email Reader
-            </motion.h2>
-            <motion.p
-              className={`projects__content--project-description ${
-                hoveredOne ? "js-color-pewter" : ""
-              }`}
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{
-                opacity: 1,
-                y: 0,
-                transition: { delay: 1.5, duration: 1, ease: "linear" },
-              }}
-              viewport={{ once: true, amount: 0.5 }}
-            >
-              Full-stack web application built on ReactJS that reads, and
-              fetches emails from the Gmail mail server using IMAP and stores
-              them in MySQL database.
-            </motion.p>
-          </div>
-        </motion.a>
-        <motion.a
           className="projects__content projects__content-portfolio"
           href="https://github.com/Tsedhondup/tsering-dhondup"
           target="_blank"
@@ -307,7 +245,7 @@ const Works = () => {
               }}
               viewport={{ once: true, amount: 0.5 }}
             >
-              Geography quiz
+              Band-Site
             </motion.h2>
             <motion.p
               className={`projects__content--project-description ${
@@ -324,6 +262,68 @@ const Works = () => {
               Showcasing my web developing skills, a video streaming web
               application built on React,  connected to the backend server via
               RESTful API built using Express in Node js.
+            </motion.p>
+          </div>
+        </motion.a>
+        <motion.a
+          className="projects__content projects__content-email-reader"
+          href="https://github.com/Tsedhondup/job-application-tracker"
+          target="_blank"
+          onMouseEnter={() => setHoveredOne(true)}
+          onMouseLeave={() => setHoveredOne(false)}
+          initial={{
+            y: 60,
+            opacity: 0,
+          }}
+          whileInView={{
+            y: 0,
+            opacity: 1,
+            transition: { delay: 0.5, duration: 1, ease: "linear" },
+          }}
+          viewport={{ once: true, amount: 0.5 }}
+        >
+          <motion.img
+            initial={{ filter: "grayScale(100)", opacity: 0 }}
+            whileInView={{
+              filter: "grayScale(0)",
+              opacity: 1,
+              transition: { delay: 0.5, duration: 1, ease: "linear" },
+            }}
+            viewport={{ once: true, amount: 0.5 }}
+            src={jobTracker}
+            alt="job-application-tracker-image"
+            className="projects__content--img projects__content--email-image "
+          />
+          <div className="projects__content--text ">
+            <motion.h2
+              className={`projects__content--project-name projects__content--text-email ${
+                hoveredOne ? "js-color-pewter" : ""
+              }`}
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: { delay: 1.5, duration: 1, ease: "linear" },
+              }}
+              viewport={{ once: true, amount: 0.5 }}
+            >
+              Email Reader
+            </motion.h2>
+            <motion.p
+              className={`projects__content--project-description ${
+                hoveredOne ? "js-color-pewter" : ""
+              }`}
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: { delay: 1.5, duration: 1, ease: "linear" },
+              }}
+              viewport={{ once: true, amount: 0.5 }}
+            >
+              Full-stack web application built on ReactJS that reads, and
+              fetches emails from the Gmail mail server using IMAP and stores
+              them in MySQL database.
             </motion.p>
           </div>
         </motion.a>
