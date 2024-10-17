7d9ee5c371ac6fe8e46bb51d2bd1ccdbd1770e0f
Tsering Dhondup <tyddhondup88@gmail.com>
2024-10-16 22:12:19 -0400

    projectData code refactored

--- src/utilities/data.js
+++ src/utilities/data.js
@@ -6,41 +6,41 @@ import bandsite from "../assets/image/band-site.jpg";
 export const projectData = [
   {
     name: "portfolio",
+    img: portfolio,
     url: "https://tseringdhondup.netlify.app/",
     repo: "https://github.com/Tsedhondup/Portfolio.git",
-    img: portfolio,
     description:
       " My Portfolio website deployed on Netlify, primarily built on React.js.",
   },
   {
     name: "brainflix",
+    img: brainflix,
     url: "https://brain-flix-app.netlify.app/",
     repo: "https://github.com/Tsedhondup/tsering-dhondup-BrainFlix.git",
-    img: brainflix,
     description:
       "Showcasing my web developing skills, a video streaming web application built on React, connected to the backend server viaRESTful API built using Express in Node js.",
   },
   {
     name: "quiz",
+    img: quiz,
     url: "https://tseringdhondupgeographyquiz.netlify.app/",
     repo: "https://github.com/Tsedhondup/Geography-quiz.git",
-    img: quiz,
     description:
       "A geography quiz is an interactive quiz game, that allows players to guess the correct answer within time allocation.",
   },
   {
     name: "band-site",
+    img: bandsite,
     url: "https://tseringdhondup-bandsite.netlify.app/",
     repo: "https://github.com/Tsedhondup/tsering-dhondup-bandsite.git",
-    img: bandsite,
     description:
       "Bandsite is a static website built as a part of the School Project at BrainStation's Software Engineering Bootcamp.",
   },
   {
     name: "email",
+    img: jobTracker,
     url: "https://github.com/Tsedhondup/EmailReader.git",
     repo: "https://github.com/Tsedhondup/EmailReader.git",
-    img: jobTracker,
     description:
       " Full-stack web application built on ReactJS that reads, and fetches emails from the Gmail mail server using IMAP and stores them in MySQL database.",
   },
