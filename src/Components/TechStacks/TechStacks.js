3b94c1c5e28f644b918375d8895e2e00544edc08
Tsering Dhondup <tyddhondup88@gmail.com>
2024-10-17 20:42:03 -0400

    style applied to skills and tools elements

--- src/Components/TechStacks/TechStacks.js
+++ src/Components/TechStacks/TechStacks.js
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
 import "./TechStacks.scss";
 import { skillsIcons } from "../../utilities/skill-icons";
 const TechStacks = () => {
@@ -23,12 +22,12 @@ const TechStacks = () => {
         viewport={{ once: true, amount: 0.5 }}
         src={item.src}
         alt={item.name}
-        className="tech-stack__tools--image"
+        className="tools__tools-content--image"
       />
     );
   });
   return (
-    <section className="tech-stacks">
+    <section className="tools">
       {/* Mobile header */}
       <motion.h2
         initial={{
@@ -44,167 +43,11 @@ const TechStacks = () => {
           },
         }}
         viewport={{ once: true, amount: 1 }}
-        className="tech-stacks__header"
+        className="tools__header"
       >
-        Primary skill sets
+        Skills and Tools
       </motion.h2>
-      <div className="tech-stacks__tools">{skillsElements}</div>
-
-      {/* <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{
-          opacity: 1,
-          transition: { duration: 1.5, ease: "linear" },
-        }}
-        viewport={{ once: true, amount: 1 }}
-        className="tech-stacks__tools"
-      >
-        <motion.span
-          initial={{ scale: 0 }}
-          whileInView={{
-            scale: 1,
-            transition: { duration: 1, ease: "linear" },
-          }}
-          viewport={{ once: true, amount: 1 }}
-          className="tech-stacks__tools--tech"
-        >
-          <FontAwesomeIcon
-            icon={faHtml5}
-            className="tech-stacks__tools--icon tech-stacks__tools--html"
-          />
-        </motion.span>
-        <motion.span
-          initial={{ scale: 0 }}
-          whileInView={{
-            scale: 1,
-            transition: { duration: 1.2, ease: "linear" },
-          }}
-          viewport={{ once: true, amount: 1 }}
-          className="tech-stacks__tools--tech"
-        >
-          <FontAwesomeIcon
-            icon={faCss3}
-            className="tech-stacks__tools--icon tech-stacks__tools--css"
-          />
-        </motion.span>
-        <motion.span
-          initial={{ scale: 0 }}
-          whileInView={{
-            scale: 1,
-            transition: { duration: 1.4, ease: "linear" },
-          }}
-          viewport={{ once: true, amount: 1 }}
-          className="tech-stacks__tools--tech"
-        >
-          <FontAwesomeIcon
-            icon={faSass}
-            className="tech-stacks__tools--icon tech-stacks__tools--sass"
-          />
-        </motion.span>
-        <motion.span
-          initial={{ scale: 0 }}
-          whileInView={{
-            scale: 1,
-            transition: { duration: 1.6, ease: "linear" },
-          }}
-          viewport={{ once: true, amount: 1 }}
-          className="tech-stacks__tools--tech"
-        >
-          <FontAwesomeIcon
-            icon={faJs}
-            className="tech-stacks__tools--icon tech-stacks__tools--js"
-          />
-        </motion.span>
-        <motion.span
-          initial={{ scale: 0 }}
-          whileInView={{
-            scale: 1,
-            transition: { duration: 1.8, ease: "linear" },
-          }}
-          viewport={{ once: true, amount: 1 }}
-          className="tech-stacks__tools--tech"
-        >
-          <FontAwesomeIcon
-            icon={faReact}
-            className="tech-stacks__tools--icon tech-stacks__tools--react"
-          />
-        </motion.span>
-        <motion.span
-          initial={{ scale: 0 }}
-          whileInView={{
-            scale: 1,
-            transition: { duration: 2, ease: "linear" },
-          }}
-          viewport={{ once: true, amount: 1 }}
-          className="tech-stacks__tools--tech"
-        >
-          <FontAwesomeIcon
-            icon={faNodeJs}
-            className="tech-stacks__tools--icon tech-stacks__tools--node"
-          />
-        </motion.span>
-
-        <motion.span
-          initial={{ scale: 0 }}
-          whileInView={{
-            scale: 1,
-            transition: { duration: 2.2, ease: "linear" },
-          }}
-          viewport={{ once: true, amount: 1 }}
-          className="tech-stacks__tools--tech"
-        >
-          <img
-            src={expressLogo}
-            alt="express-logo"
-            className="tech-stacks__tools--img tech-stacks__tools--express"
-          />
-        </motion.span>
-        <motion.span
-          initial={{ scale: 0 }}
-          whileInView={{
-            scale: 1,
-            transition: { duration: 2.4, ease: "linear" },
-          }}
-          viewport={{ once: true, amount: 1 }}
-          className="tech-stacks__tools--tech"
-        >
-          <img
-            src={mysqlLogo}
-            alt="mysql-logo"
-            className="tech-stacks__tools--img tech-stacks__tools--mysql"
-          />
-        </motion.span>
-        <motion.span
-          initial={{ scale: 0 }}
-          whileInView={{
-            scale: 1,
-            transition: { duration: 2.8, ease: "linear" },
-          }}
-          viewport={{ once: true, amount: 1 }}
-          className="tech-stacks__tools--tech"
-        >
-          <img
-            src={knexLogo}
-            alt="knex-logo"
-            className="tech-stacks__tools--img tech-stacks__tools--knex"
-          />
-        </motion.span>
-        <motion.span
-          initial={{ scale: 0 }}
-          whileInView={{
-            scale: 1,
-            transition: { duration: 2.8, ease: "linear" },
-          }}
-          viewport={{ once: true, amount: 1 }}
-          className="tech-stacks__tools--tech"
-        >
-          <img
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
-            alt="knex-logo"
-            className="tech-stacks__tools--img tech-stacks__tools--knex"
-          />
-        </motion.span>
-      </motion.div> */}
+      <div className="tools__tools-content">{skillsElements}</div>
     </section>
   );
 };
